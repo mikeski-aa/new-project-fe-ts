cfb39bf4bd325afb42121480aa4a8f225e207c32
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-21 13:12:58 +0200

    Setting up login and register pages first

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import "../styles/loginregister.css";
+
+import React from "react";
+
+function Login() {
+  return (
+    <div className="loginContainer">
+      <div className="loginMainDiv">Bla bla</div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ utils/urlConst.ts
@@ -0,0 +1,3 @@
+const LOCAL_URL: string = "http://localhost:5001/api/";
+
+export { LOCAL_URL };
