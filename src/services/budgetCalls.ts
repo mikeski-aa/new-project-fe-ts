adb5f9533ba4fa23e4ce1c4bc930bf9eae1da248
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-23 18:22:55 +0200

    Create interface for budget object returning

--- src/services/budgetCalls.ts
+++ src/services/budgetCalls.ts
@@ -6,24 +6,44 @@ const headerInfo: HeadersInit = {
   Authorization: "Bearer " + localStorage.getItem("token"),
 };
 
-async function getBudgets(userId: number | null | undefined) {
+interface IItem {
+  id: number;
+  budgetId: number;
+  name: string;
+  price: number;
+  date: string;
+}
+
+interface IBudget {
+  budgetValue: number;
+  customInterval: number | null | undefined;
+  id: number;
+  name: string;
+  userId: number;
+  item: IItem;
+}
+
+async function getBudgets(
+  userId: number | null | undefined
+): Promise<IBudget | void> {
   const url = LOCAL_URL + `/budgets?userid=${userId}`;
 
   try {
     const response = await fetch(url, { method: "GET", headers: headerInfo });
 
     if (!response.ok) {
-      return console.log(response);
+      console.log(response);
+      return;
     }
 
-    const json = response.json();
+    const json: IBudget = await response.json();
 
     console.log(json);
 
     return json;
   } catch (error) {
     console.log(error);
-    return error;
+    return;
   }
 }
 
