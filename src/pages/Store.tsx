6ae20081eb87f3f79a6453fa0b3828fbe8d918c8
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-24 19:16:13 +0200

    Working on assigning stores

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,24 @@
 import { useParams } from "react-router-dom";
 import "../styles/store.css";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../App";
+import { IStore } from "../interfaces/userContextInterfaces";
 
 function Store() {
-  const { id } = useParams();
-  console.log(id);
+  const userContext = useContext(UserContext);
+  const { paramsId } = useParams();
+
+  useEffect(() => {
+    // const shallowCopy: IStore[] = [...userContext.user];
+    // const filteredArray: IStore[] = shallowCopy.filter(
+    //   (item) => item.id != +paramsId
+    // );
+
+    console.log(userContext.stores);
+
+    console.log("xd");
+  }, []);
+
   return (
     <div className="storePageContainer">
       <h1 className="storeName">Placeholder name</h1>
