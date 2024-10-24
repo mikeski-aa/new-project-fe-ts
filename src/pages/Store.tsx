fac98e44b09e451012637031b2a7a3703d4923d8
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-24 18:59:53 +0200

    Added params to get store id

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,12 @@
+import { useParams } from "react-router-dom";
 import "../styles/store.css";
 
 function Store() {
+  const { id } = useParams();
+  console.log(id);
   return (
-    <div className="storeContainer">
-      <div className="storeName">Placeholder name</div>
+    <div className="storePageContainer">
+      <h1 className="storeName">Placeholder name</h1>
       <div className="store items">Placeholder items</div>
     </div>
   );
