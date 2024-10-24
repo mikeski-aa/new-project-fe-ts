e24cd8283210058e4b4f71d69289a020195dd266
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-24 18:12:12 +0200

    Added more to store cards

--- src/components/StoreHolder.tsx
+++ src/components/StoreHolder.tsx
@@ -4,13 +4,19 @@ import IndividualProduct from "./IndividualProduct";
 
 function StoreHolder({ store }: { store: IStore }) {
   return (
-    <div className="budgetContainer">
+    <div className="storeContainer">
       <div className="budgetHeading">{store.name}</div>
       <div className="budgetValue">{store.location}</div>
       <div className="productHolder">
-        {store.products.map((product, index) => (
+        Unique products in store: {store.products.length}
+        {/* {store.products.map((product, index) => (
           <IndividualProduct key={index} product={product} />
-        ))}
+        ))} */}
+      </div>
+      <div className="storeButtons">
+        <button className="storeBtn">Open</button>
+        <button className="storeBtn">Edit</button>
+        <button className="storeBtn">Delete</button>
       </div>
     </div>
   );
