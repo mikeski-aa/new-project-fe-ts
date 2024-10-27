c58248ef643d2c8d7255d0cf6bf9f957c1c80080
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-27 18:04:54 +0100

    Working on search result show

--- src/components/EndOfDayReport.tsx
+++ src/components/EndOfDayReport.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, SyntheticEvent, useState } from "react";
 import "../styles/endofdayreport.css";
 import { convertDate } from "../utils/dateConversion";
 import { IProduct } from "../interfaces/userContextInterfaces";
@@ -20,6 +20,24 @@ function EndOfDayReport({
     setModal(false);
   };
 
+  // check difference between
+  //   const target = e.target as HTMLInputElement;
+  //   and
+  //   const target:HTMLInputElement = e.target;
+  const handleInputChange = (e: SyntheticEvent): void => {
+    const target = e.target as HTMLInputElement;
+    const shallowProducts = [...products];
+    const tempArray = [];
+
+    for (let x = 0; x < shallowProducts.length; x++) {
+      if (shallowProducts[x].sku.includes(target.value)) {
+        tempArray.push(shallowProducts[x]);
+      }
+    }
+
+    setSearchList(tempArray);
+  };
+
   return (
     <div className={modal ? "eodModal show" : "eodModal hide"}>
       <div className="eodMainContainer">
@@ -34,8 +52,10 @@ function EndOfDayReport({
             className="itemSearchInput"
             type="string"
             placeholder="Name or SKU"
+            onChange={(e) => handleInputChange(e)}
           ></input>
         </div>
+        <div className="searchResultContainer"></div>
       </div>
     </div>
   );
