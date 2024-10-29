6c60c56e05d5c2464b53fc3ef1547daab36e9c7e
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-29 17:10:29 +0100

    Working on array rework

--- src/utils/reportConversions.ts
+++ src/utils/reportConversions.ts
@@ -4,15 +4,27 @@ import { IReport } from "../interfaces/userContextInterfaces";
 // goes through each item, checks date, creates corresponding month
 // needs to after that assign each report to each existing month
 // order in asc order for months and items inside
+
+interface IMonth {
+  name: string;
+  reports: IReport[];
+}
+
 function getMonthsFromReports(reports: IReport[]) {
-  const monthArray = [];
+  const monthArray: IMonth[] = [];
+  const testArray: string[] = [];
 
   for (let x = 0; x < reports.length; x++) {
     const tempDate = new Date(reports[x].date);
-    monthArray.push(tempDate.toLocaleString("default", { month: "long" }));
+    const monthName = tempDate.toLocaleString("default", { month: "long" });
+    if (testArray.includes(monthName)) {
+      console.log("duplicate found!");
+    } else {
+      testArray.push(monthName);
+    }
   }
 
-  console.log(monthArray);
+  console.log(testArray);
 }
 
 export { getMonthsFromReports };
