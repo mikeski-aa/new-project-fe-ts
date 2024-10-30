1a6a6cb117f876f5354e2c850b567327fd0643e3
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-30 11:24:29 +0100

    Working on modal to open specific reprot info

--- src/components/ReportCalendarReportElement.tsx
+++ src/components/ReportCalendarReportElement.tsx
@@ -4,9 +4,13 @@ function ReportCalendarReportElement({ reportItem }: { reportItem: IReport }) {
   const dateConversion = new Date(reportItem.date);
   const day = dateConversion.getDate();
 
+  const handleDayClick = () => {};
   return (
     <div className="indReportContainer">
-      <button>{`${day}`}</button>
+      <button
+        onClick={handleDayClick}
+        className="calendarDayBtn"
+      >{`${day}`}</button>
       {/* <div className="reportItem">{`${day}`}</div>
       <div className="reportItem">{`Products sold: ${reportItem.soldProducts}`}</div>
       <div className="reportItem">{`Total sales: $${reportItem.totalSaleValue}`}</div> */}
