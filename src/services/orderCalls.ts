1b950f934a2a2d960768c0e5605418ec297e8eba
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-31 12:31:18 +0100

    Created temp service call to new endpoint

---
+++ src/services/orderCalls.ts
@@ -0,0 +1,38 @@
+import { IProduct } from "../interfaces/userContextInterfaces";
+import { LOCAL_URL } from "../utils/urlConst";
+
+const getHeaderInfo = (): HeadersInit => {
+  return {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  };
+};
+
+async function createOrder(orderProducts: IProduct[]): Promise<boolean> {
+  const url = LOCAL_URL + "orders";
+
+  const newBody = {
+    orderProducts: orderProducts,
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: getHeaderInfo(),
+      body: JSON.stringify(newBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error validating request body`);
+    }
+
+    const json: boolean = await response.json();
+
+    return json;
+  } catch (error) {
+    throw new Error(`Server or network error`);
+  }
+}
+
+export { createOrder };
