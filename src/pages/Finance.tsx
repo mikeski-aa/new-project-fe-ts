6956ca4e75a1b05c3e204723f332865f5d616e00
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-02 17:08:10 +0100

    Working on store order items

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -1,17 +1,40 @@
 import "../styles/finance.css";
 import { UserContext } from "../App";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getOrdersForStore } from "../services/orderCalls";
+import { IStore } from "../interfaces/userContextInterfaces";
+
+export interface IOrderItem {
+  id: number;
+  orderId: number;
+  quantityordered: number;
+  sku: string;
+}
+
+export interface IOrder {
+  id: number;
+  storeId: number;
+  date: Date;
+  totalvalue: number;
+  itemsordered: IOrderItem[];
+}
 
 function Finance() {
   const userContext = useContext(UserContext);
+  const [activeStore, setActiveStore] = useState<string>("");
+  const [activeOrders, setActiveOrders] = useState<IStore>();
 
   useEffect(() => {
     console.log(userContext.stores);
+    if (userContext && userContext.stores.length != 0) {
+      setActiveStore(userContext.stores[0].name);
+    }
   }, [userContext.stores]);
 
-  const handleStoreClick = async (id: number) => {
-    await getOrdersForStore(id);
+  const handleStoreClick = async (id: number, name: string) => {
+    setActiveStore(name);
+    const xd = await getOrdersForStore(id);
+    console.log(xd);
   };
 
   return (
@@ -23,14 +46,19 @@ function Finance() {
             ? userContext?.stores.map((store, index) => (
                 <button
                   key={index}
-                  className={`finStoreBtn`}
-                  onClick={() => handleStoreClick(store.id)}
+                  className={
+                    activeStore === store.name
+                      ? `finStoreBtn active`
+                      : `finStoreBtn`
+                  }
+                  onClick={() => handleStoreClick(store.id, store.name)}
                 >
                   {store.name}
                 </button>
               ))
             : null}
         </div>
+        <div className="reportHolder"></div>
       </div>
     </div>
   );
